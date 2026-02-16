9208d7283296153d283e7e68224c83429d43d18c
Imashi Sanjana Amarathunga <imashi.20232211@iit.ac.lk>
2026-02-16 11:36:22 +0530

    Create Sign up page Structure
    without social icons

---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,65 @@
+ 
+
+export default function Signup() {
+    return (
+        <div className="min-h-screen flex items-center justify-center px-6"> 
+
+            <div className="w-full max-w-6xl h-[520px] rounded-2xl flex border">
+
+                {/* Left Side */}
+                <div className="w-1/2 p-10 flex-col justify-center border-r">
+
+                    <h1 className="text-3xl font-semibold">
+                        Welcome to Orean360
+                    </h1>
+
+                    <p className="mt-4">
+                        Simplify your workflow, social media, and trilingual AI engagement in one smart platform.
+                    </p>
+
+                </div>
+
+                {/* Right Side */}
+
+                <div className="w-1/2 p-10 flex flex-col justify-center">
+
+                    <h2 className="text-2xl font-semibold mb-6">
+                        Create an Account
+                    </h2>
+
+                    <form className="space-y-4">
+
+                        <input 
+                        type="text" 
+                        placeholder="Email"
+                        className="w-full px-4 py-2 border rounded"
+                        />
+
+                        <input 
+                        type="text" 
+                        placeholder="password"
+                        className="w-full px-4 py-2 border rounded"
+                        />
+
+                        <input 
+                        type="text" 
+                        placeholder="Workplace Name"
+                        className="w-full px-4 py-2 border rounded"
+                        />
+
+                        <button
+                        type="submit"
+                        className="w-full py-2 border rounded">
+                        Sign Up
+                        </button>
+                    </form>
+
+
+                </div>
+            </div>
+
+        </div>
+
+    );
+
+}
