5bc8455166cdd1d54df9bb6bf8211c07ceb5b9d3
Imashi Sanjana Amarathunga <imashi.20232211@iit.ac.lk>
2026-02-20 22:12:59 +0530

    Style left panel text with gradient and italic font

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,94 +1,65 @@
-import { Facebook, Instagram, Linkedin } from "lucide-react";
+"use client";
 
+import Image from "next/image";
+import { Facebook, Instagram, Linkedin} from "lucide-react" ;
+import { Sora,Inter } from "next/font/google";
+
+const sora = Sora({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+});
+
+const inter = Inter({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    style: ["normal", "italic"],
+});
  
 
-export default function Signup() {
+export default function SignupPage() {
     return (
-        <div className="min-h-screen flex items-center justify-center px-6"> 
-
-            <div className="w-full max-w-6xl h-[520px] rounded-2xl flex border">
-
-                {/* Left Side */}
-                <div className="w-1/2 p-10 flex-col justify-center border-r">
-
-                    <h1 className="text-3xl font-semibold">
-                        Welcome to Orean360
-                    </h1>
-
-                    <p className="mt-4">
-                        Simplify your workflow, social media, and trilingual AI engagement in one smart platform.
-                    </p>
-
-                </div>
-
-                {/* Right Side */}
-
-                <div className="w-1/2 p-10 flex flex-col justify-center">
-
-                    <h2 className="text-2xl font-semibold mb-6">
-                        Create an Account
-                    </h2>
-
-                    <form className="space-y-4">
-
-                        <input 
-                        type="text" 
-                        placeholder="Email"
-                        className="w-full px-4 py-2 border rounded"
-                        />
-
-                        <input 
-                        type="text" 
-                        placeholder="password"
-                        className="w-full px-4 py-2 border rounded"
-                        />
-
-                        <input 
-                        type="text" 
-                        placeholder="Workplace Name"
-                        className="w-full px-4 py-2 border rounded"
-                        />
-
-                        <div className="flex items-center gap-4 my-6 text-white/60">
-                            <div className="flex-1 h-[1px] bg-white/30" />
-                            <span className="text-sm">or</span>
-                            <div className="flex-1 h-[1px] bg-white/30" />
-                        </div>
-
-                        <div className="flex justify-center gap-6 my-4">
-
-                            <div className="w-12 h-12 rounded-full bg-white/20 flex items-center justify-center hover:bg-white/30 transition cursor-pointer">
-                                <Facebook size={20} />
-                            </div>
-
-                            <div className="w-12 h-12 rounded-full bg-white/20 flex items-center justify-center hover:bg-white/30 transition cursor-pointer">
-                                <span className="font-bold text-lg">G</span>
-                            </div>
-
-                            <div className="w-12 h-12 rounded-full bg-white/20 flex items-center justify-center hover:bg-white/30 transition cursor-pointer">
-                                <Instagram size={20} />
-                            </div>
-
-                            <div className="w-12 h-12 rounded-full bg-white/20 flex items-center justify-center hover:bg-white/30 transition cursor-pointer">
-                                <Linkedin size={20} />
-                            </div>
-
-                        </div>
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#0b1a3a] via-[#0E1F4D] TO-[#020617] PX-6">
 
 
-                        <button
-                        type="submit"
-                        className="w-full py-2 border rounded">
-                        Sign Up
-                        </button>
-                    </form>
+            {/* Left Side - Illustration and Text */}
+            <div className="relative w-full max-w-4xl h-[520px] rounded-4xl overflow-hidden shadow-2xl flex">
+
+                <div className="relative top-0 h-full w-[400px] bg-gradient-to-br from-blue-500/50 via-cyan-500/20 to-black-500/80 rounded-r-[100px]" />
+
+                    <div className="absolute top-0 left-2 z-10">
+                        <Image 
+                        src = "/logo.png" alt="Illustration"
+                        width={100} height={40}
+                        className="object-contain"/>
+                    </div>
+
+                    <div className="absolute z-10 mt-[310px] px-8 bg-gradient-to-r from-[#355872] via-[#f5fee5] to-[#355872] 
+                    bg-clip-text text-transparent"> 
+                        <h1 className={`${sora.className} text-4xl leading-none mb-4  `}>
+                            Welcome to <br /> Orean360
+                        </h1>
+                    </div>
+
+                    <div className="absolute z-10 mt-[350px] px-10 bg-gradient-to-r from-[#9CD5FF] via-[#f5fee5] to-[#9CD5FF] 
+                    bg-clip-text text-transparent ">
+                        <p className={`${inter.className} text-sm font-light italic leading-tight mt-[46px] px-0`}>
+                            Simplify your workflow, social media, <br />and trilingual AI engagement in one smart platform.
+                        </p>
+                    </div>
 
 
-                </div>
+
+                 
+                     
+
+                         
+
+                    
             </div>
-
         </div>
+ 
 
-    );
+         
+    )
 
 }
